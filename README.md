cfc061b7056b0ce198a00d1c9ba6c8d1c8efe972
mattrudin <matthias.rudin@live.com>
2018-10-08 07:28:57 +0200

    Updated lesson02

--- README.md
+++ README.md
@@ -57,3 +57,21 @@ toggleBoolean() {
 }
 ```
 The const currentBoolean contains the current boolean and after that, the state will be set with the opposite of the current boolean.
+
+### Lesson 2: Compound Components
+#### static
+[MDN: static](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/static)  
+[Medium: Understanding static in JS](https://medium.com/front-end-hacking/understanding-static-in-javascript-10782149993)  
+[Youtube: Static Methods in JavaScript Tutorial](https://www.youtube.com/watch?v=10b6K9fORI4)  
+Examples:
+* Utility methods can be static (best practice; see youtube)
+* only the class has the static methods, instances have not (see medium)
+
+#### this.props.children: React.Children.map & React.cloneElement
+[mxstbr: A deep dive into children in React](https://mxstbr.blog/2017/02/react-children-deepdive/)  
+[learn.co: this.props.children](https://learn.co/lessons/react-this-props-children)  
+```
+*React.Children.map* has two parameters: the first one is the children themselves, and the second one is a function that transforms the value of the child. In this case, we're adding an extra prop. We do that using *React.cloneElement*. As the first argument we pass in the child component itself, and as the second argument, we pass in any additional props. Those additional props get merged with the child's existing props, overwriting any props with the same key.
+```
+[Hashrocket: Mapping Over One Or Many Children In React](https://til.hashrocket.com/posts/yb1ee3dhxp-mapping-over-one-or-many-children-in-react)  
+[Github: Dynamically Add Props To A Child Component](https://github.com/jbranchaud/til/blob/master/react/dynamically-add-props-to-a-child-component.md)  
--- src/exercises/02.js
+++ src/exercises/02.js
@@ -4,18 +4,9 @@ import React from 'react'
 import {Switch} from '../switch'
 
 class Toggle extends React.Component {
-  // you can create function components as static properties!
-  // for example:
-  // static Candy = (props) => <div>CANDY! {props.children}</div>
-  // Then that could be used like: <Toggle.Candy />
-  // This is handy because it makes the relationship between the
-  // parent Toggle component and the child Candy component more explicit
-  // 🐨 You'll need to create three such components here: On, Off, and Button
-  //    The button will be responsible for rendering the <Switch /> (with the right props)
-  // 💰 Combined with changes you'll make in the `render` method, these should
-  //    be able to accept `on`, `toggle`, and `children` as props.
-  //    Note that they will _not_ have access to Toggle instance properties
-  //    like `this.state.on` or `this.toggle`.
+  static On = (props) => props.on ? props.children : null;
+  static Off = (props) => props.on ? null : props.children;
+  static Button = ({ on, toggle }) => <Switch on={on} onClick={toggle} />
   state = {on: false}
   toggle = () =>
     this.setState(
@@ -23,18 +14,12 @@ class Toggle extends React.Component {
       () => this.props.onToggle(this.state.on),
     )
   render() {
-    // we're trying to let people render the components they want within the Toggle component.
-    // But the On, Off, and Button components will need access to the internal `on` state as
-    // well as the internal `toggle` function for them to work properly. So here we can
-    // take all this.props.children and make a copy of them that has those props.
-    //
-    // To do this, you can use:
-    // 1. React.Children.map: https://reactjs.org/docs/react-api.html#reactchildrenmap
-    // 2. React.cloneElement: https://reactjs.org/docs/react-api.html#cloneelement
-    //
-    // 🐨 you'll want to completely replace the code below with the above logic.
-    const {on} = this.state
-    return <Switch on={on} onClick={this.toggle} />
+    return React.Children.map(this.props.children, childElement => {
+      return React.cloneElement(childElement, {
+        on: this.state.on,
+        toggle: this.toggle
+      })
+    })
   }
 }
 
